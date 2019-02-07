64192b394d581218beaf87334cdaf99301a63f0f
alumno Secundaria <al3201@cedi.local>
2019-02-07 09:16:11 -0600

    ya jaló

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+inmuebles= [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
+      this.http.get('/v1/api/pin-data?url=/s-renta-inmuebles/guadalajara-y-zona-metro/v1c1098l10567p1&geo=(21.10631012145462,-102.42214381725364),(20.21712862656199,-104.32387728274637)')
+      .subscribe(data => {
+        console.log(JSON.stringify(data['ads'],null,3));
+        if(data.hasOwnProperty('ads')){
+          this.inmuebles=data['ads'];
+        }
+      }, error => {
+        console.log(JSON.stringify(error));
+      })
 
   }
 
 }
+//PRECIO DEL INMUEBLE ESTA EN ADS, DESPUES EN PRICE, Y FORMATTEDAMOUNT. LA UBICACION ESTA EN GEO, Y EN ATRIBUTO NAME. LA IMAGEN ESTA EN EL ARREGLO PICTURES EN LA POSICIÓN 0 
